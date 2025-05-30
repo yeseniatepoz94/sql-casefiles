c3ccb58c42a9e3ff037edff524994921e979be41
yeseniatepoz94 <tepozyesenia@gmail.com>
2025-05-30 12:31:47 -0400

    Add 01.01_select_basics.sql - pull customer contact info

--- 01_foundations/01.01_select_basics.sql
+++ 01_foundations/01.01_select_basics.sql
@@ -1,1 +1,15 @@
+-- Prompt:
+-- As a customer success analyst, you’re asked to pull a list of customers 
+-- for an email campaign. Include their first name, last name, and email.
 
+-- Assumptions:
+-- The database has a table named `customers`
+-- with columns: first_name, last_name, email
+
+-- SQL Solution:
+SELECT
+  first_name,
+  last_name,
+  email
+FROM
+  customers;
